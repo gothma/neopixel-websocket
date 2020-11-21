94a9dd218f72730cb6da09d12c4d74ef374088dd
Martin Goth <git@ziisch.de>
2020-11-21 11:03:21 +0100

    Response handling

--- client.py
+++ client.py
@@ -15,6 +15,7 @@ class WebsocketSwitch():
                 on_close=lambda ws: self.on_close())
         self.ws.on_open=lambda ws: self.setup()
         self.led = led.Strip()
+        self.value = False
         self.ws.run_forever()
 
     def setup(self):
@@ -30,28 +31,40 @@ class WebsocketSwitch():
         self.send('remove', dict(name=name))
 
     def on_set(self, payload):
-        if self.name != payload['name']:
-            return
         if 'On' != payload['characteristic']:
             return
         if payload['value']:
             self.thread = threading.Thread(target=lambda: self.led.rainbow_cycle(), args=())
             self.thread.start()
-            print("Started")
+            self.value = True
         else:
             self.led.fill((0,0,0))
+            self.value = False
+
+    def on_get(self, payload):
+        if 'On' != payload['characteristic']:
+            return
+        self.send('callback',
+                {"name": self.name,
+                 "characteristic": "On",
+                 "value": self.value})
+
 
     def on_message(self, raw_message):
         message = json.loads(raw_message)
         reaction = {
+                'get': lambda p: self.on_get(p),
                 'set': lambda p: self.on_set(p)}.get(message['topic'], None)
+
         if reaction is None:
             print('Unknown response', message)
             return
 
-        reaction(message['payload'])
+        if message['payload']['name'] == self.name:
+            reaction(message['payload'])
 
-    def on_close():
+    def on_close(self):
+        self.led.fill((0,0,0))
         self.remove(self.name)
 
 switch = WebsocketSwitch('ws://localhost:4050', 'LED')
